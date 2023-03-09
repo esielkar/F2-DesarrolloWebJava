1814485b435808959ef5907ec82440005f5ee683
Esiel Arizmendi <esiel_kar@hotmail.com>
2023-03-09 09:34:27 -0600

    [F2][S4] - Postwork 4
    Implementación del postwork 4

---
+++ postwork_4/src/test/java/com/equipo4/postwork_4/Postwork4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo4.postwork_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Postwork4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
