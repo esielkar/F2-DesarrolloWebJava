a2184a14f11ea0b4113cd68121432e7dc4b7d38b
Esiel Arizmendi <esiel_kar@hotmail.com>
2023-03-14 21:29:45 -0600

    Creación de postworks
    Se añade la carpeta general de la unión de todos los postworks para el entregable,

---
+++ postworks/src/main/java/com/equipo4/postworks/PostworksApplication.java
@@ -0,0 +1,13 @@
+package com.equipo4.postworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostworksApplication.class, args);
+	}
+
+}
---
+++ postworks/src/test/java/com/equipo4/postworks/PostworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo4.postworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
